d38944d7fcb33467f0d8862cd8de8179b3dc9d86
jaehyunChoi <chlwougs180@name.com>
2025-01-16 23:09:01 +0900

    Changes

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    // project initialization
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
